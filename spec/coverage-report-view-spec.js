5e875de3350908ab8eb57134acdbd749fef6afe1
Kentaro Matsushita <kentaro.m811@gmail.com>
2017-06-10 13:21:44 +0900

    Initial commit

---
+++ spec/coverage-report-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CoverageReportView from '../lib/coverage-report-view';
+
+describe('CoverageReportView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
