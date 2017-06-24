e4fc6742ae1abc8b7fd9d118591adaa11a567132
Kentaro Matsushita <kentaro.m811@gmail.com>
2017-06-24 20:26:50 +0900

    Delete unused files.

--- spec/coverage-report-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CoverageReportView from '../lib/coverage-report-view';
-
-describe('CoverageReportView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
