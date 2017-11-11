d5fcef7e94391d67de43723edc7b89313b86a020
Kentaro Matsushita <kentaro.m811@gmail.com>
2017-11-11 10:14:07 +0900

    Fix unit test.

--- test/lcov-watcher.test.js
+++ test/lcov-watcher.test.js
@@ -1,25 +1,26 @@
-import LcovWatcher from '../lib/lcov-watcher';
-import { TextEditor } from 'atom';
 import path from 'path';
+import { CompositeDisposable } from 'event-kit';
+
+import LcovWatcher from '../lib/lcov-watcher';
 
 /* global describe, it, beforeEach, assert */
 
 describe('LcovWatcher', () => {
-  let editor;
+  let subscriptions;
 
   beforeEach(() => {
-    editor = new TextEditor();
+    subscriptions = new CompositeDisposable();
   });
 
   it('Create LcovWatcher instance', () => {
     const lcovPath = path.resolve(__dirname, 'fixture/dummy_lcov.info');
-    const lcovWatcher = new LcovWatcher(editor, lcovPath);
+    const lcovWatcher = new LcovWatcher(lcovPath, subscriptions);
     assert.instanceOf(lcovWatcher, LcovWatcher);
   });
 
   it('Watch the Lcov file', () => {
     const lcovPath = path.resolve(__dirname, 'fixture/dummy_lcov.info');
-    const lcovWatcher = new LcovWatcher(editor, lcovPath);
+    const lcovWatcher = new LcovWatcher(lcovPath, subscriptions);
     lcovWatcher.watch();
     assert.isNotNull(lcovWatcher.watcher);
     lcovWatcher.unwatch();
@@ -27,7 +28,7 @@ describe('LcovWatcher', () => {
 
   it('Unwatch the Lcov file', () => {
     const lcovPath = path.resolve(__dirname, 'fixture/dummy_lcov.info');
-    const lcovWatcher = new LcovWatcher(editor, lcovPath);
+    const lcovWatcher = new LcovWatcher(lcovPath, subscriptions);
     lcovWatcher.watch();
     lcovWatcher.unwatch();
     assert.isNull(lcovWatcher.watcher);
