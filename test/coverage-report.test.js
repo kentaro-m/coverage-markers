feb0d132b2845cd73dcf13eb8d80946fc313f1ef
Kentaro Matsushita <kentaro.m811@gmail.com>
2017-11-26 14:27:13 +0900

    Fix coverage report unit test.

--- test/coverage-report.test.js
+++ test/coverage-report.test.js
@@ -1,7 +1,16 @@
 import Promise from 'bluebird';
 import sinon from 'sinon';
 import { TextEditor } from 'atom';
-import { findCoverageReportFilePath, renderCoverageMarkers, removeCoverageMarkers, __RewireAPI__ as CoverageReportRewireAPI } from '../lib/coverage-report';
+import Marker from '../lib/marker';
+import * as CoverageReport from '../lib/coverage-report';
+
+const {
+  findCoverageReportFilePath,
+  removeCoverage,
+  findCoverageReportForActiveEditor,
+  readCoverageReportFile,
+  convertCoverageReportForVisibleEditors,
+} = CoverageReport;
 
 const glob = require('glob');
 
@@ -31,7 +40,7 @@ describe('CoverageReport', () => {
       return assert.eventually.equal(findCoverageReportFilePath('/test-project'), '/test-project/app/coverage/lcov.info');
     });
 
-    it('Throw error, when the coverage report is not found', () => {
+    it('Throw the error, when the coverage report is not found', () => {
       globStub.callsFake(() => {
         return Promise.resolve([]);
       });
@@ -40,89 +49,98 @@ describe('CoverageReport', () => {
     });
   });
 
-  describe('renderCoverageMarkers', () => {
-    beforeEach(() => {
-      CoverageReportRewireAPI.__Rewire__('readCoverageReportFile', () => {
-        return Promise.resolve('hoge');
-      });
-
-      const coverageRecords = [{
-        sourceFile: '/test-project/index.js',
-        lines: {
-          data: [
-            { checksum: '', executionCount: 0, lineNumber: 16 },
-            { checksum: '', executionCount: 1, lineNumber: 17 },
-            { checksum: '', executionCount: 0, lineNumber: 22 },
-            { checksum: '', executionCount: 1, lineNumber: 29 },
-            { checksum: '', executionCount: 0, lineNumber: 33 },
-          ],
-        },
-      }];
-
-      CoverageReportRewireAPI.__Rewire__('convertCoverageReportForVisibleEditors', () => {
-        return coverageRecords;
-      });
-
-      CoverageReportRewireAPI.__Rewire__('findCoverageReportForActiveEditor', () => {
-        return coverageRecords;
-      });
-    });
-
-    afterEach(() => {
-      CoverageReportRewireAPI.__ResetDependency__('readCoverageReportFile');
-      CoverageReportRewireAPI.__ResetDependency__('convertCoverageReportForVisibleEditors');
-      CoverageReportRewireAPI.__ResetDependency__('findCoverageReportForActiveEditor');
-    });
-
-    it('Render markers based on the active editor.', async () => {
-      const editor = new TextEditor();
-      await renderCoverageMarkers(editor, 'hoge');
-      const markers = editor.getMarkers();
-      assert.isAbove(markers.length, 0);
+  describe('readCoverageReportFile', () => {
+    it('Throw the error, when the coverage file path is invalid', () => {
+      assert.isRejected(readCoverageReportFile('/test-project/lcov.info', 'coverage report file could not be read.'));
     });
   });
 
-  describe('removeCoverageMarkers', () => {
+  describe('convertCoverageReportForVisibleEditors', () => {
+    it('Get the coverage report, when the coverage file is valid', async () => {
+      const coverageFile = `
+        TN:
+        SF:/test-project/index.js
+        DA:16,0
+        DA:17,0
+        DA:22,0
+        DA:29,0
+        DA:33,0
+        LF:7
+        LH:0
+        BRF:0
+        BRH:0
+        end_of_record`;
+
+        const coverageReport = convertCoverageReportForVisibleEditors(coverageFile);
+        assert.equal(coverageReport[0].sourceFile, '/test-project/index.js');
+    });
+
+    it('Throw the error, when the coverage file is invalid', () => {
+      const coverageFile = `
+        TN:
+        DA:16,0
+        DA:17,0
+        DA:22,0
+        DA:29,0
+        DA:33,0
+        LF:7
+        LH:0
+        BRF:0
+        BRH:0
+        end_of_record`;
+
+        assert.throws(() => {
+          convertCoverageReportForVisibleEditors(coverageFile);
+        }, 'coverage report could not be converted.');
+    });
+  });
+
+  describe('findCoverageReportForActiveEditor', () => {
+    it('Get the coverage report for active editor, when the coverage report is valid', () => {
+      const coverageRecords = [
+        {
+          sourceFile: '/test-project/foo.js',
+          lines: {
+            data: [
+              { checksum: '', executionCount: 0, lineNumber: 16 },
+              { checksum: '', executionCount: 1, lineNumber: 17 },
+              { checksum: '', executionCount: 0, lineNumber: 22 },
+              { checksum: '', executionCount: 1, lineNumber: 29 },
+              { checksum: '', executionCount: 0, lineNumber: 33 },
+            ],
+          },
+        },
+        {
+          sourceFile: '/test-project/bar.js',
+          lines: {
+            data: [
+              { checksum: '', executionCount: 0, lineNumber: 16 },
+              { checksum: '', executionCount: 1, lineNumber: 17 },
+              { checksum: '', executionCount: 0, lineNumber: 22 },
+              { checksum: '', executionCount: 1, lineNumber: 29 },
+              { checksum: '', executionCount: 0, lineNumber: 33 },
+            ],
+          },
+        },
+      ];
+
+      const result = findCoverageReportForActiveEditor('/test-project/bar.js', coverageRecords);
+      assert.equal(result[0].sourceFile, '/test-project/bar.js');
+    });
+  });
+
+  describe('removeCoverage', () => {
     let editor;
 
     beforeEach(async () => {
-      CoverageReportRewireAPI.__Rewire__('readCoverageReportFile', () => {
-        return Promise.resolve('hoge');
-      });
-
-      const coverageRecords = [{
-        sourceFile: '/test-project/index.js',
-        lines: {
-          data: [
-            { checksum: '', executionCount: 0, lineNumber: 16 },
-            { checksum: '', executionCount: 1, lineNumber: 17 },
-            { checksum: '', executionCount: 0, lineNumber: 22 },
-            { checksum: '', executionCount: 1, lineNumber: 29 },
-            { checksum: '', executionCount: 0, lineNumber: 33 },
-          ],
-        },
-      }];
-
-      CoverageReportRewireAPI.__Rewire__('convertCoverageReportForVisibleEditors', () => {
-        return coverageRecords;
-      });
-
-      CoverageReportRewireAPI.__Rewire__('findCoverageReportForActiveEditor', () => {
-        return coverageRecords;
-      });
-
       editor = new TextEditor();
-      await renderCoverageMarkers(editor, 'hoge');
-    });
-
-    afterEach(() => {
-      CoverageReportRewireAPI.__ResetDependency__('readCoverageReportFile');
-      CoverageReportRewireAPI.__ResetDependency__('convertCoverageReportForVisibleEditors');
-      CoverageReportRewireAPI.__ResetDependency__('findCoverageReportForActiveEditor');
+      const marker = new Marker(editor);
+      marker.addTestCovered(2);
+      marker.addTestUncovered(5);
     });
 
     it('Remove all markers from visible editors.', () => {
-      removeCoverageMarkers(editor);
+      removeCoverage(editor);
       const markers = editor.getMarkers();
       assert.equal(markers.length, 0);
     });
